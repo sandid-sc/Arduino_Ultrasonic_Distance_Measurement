55e9801d585f437efcbd5168c38c876635b5606b
Tasko99 <tmtv9001@gmail.com>
2021-12-10 20:44:42 +0330

    Update UltrasonicDistanceSensor.ino

--- UltrasonicDistanceSensor/UltrasonicDistanceSensor.ino
+++ UltrasonicDistanceSensor/UltrasonicDistanceSensor.ino
@@ -1,19 +1,15 @@
-/* Initial version*/
-#include <SRF05.h>
 #define trg 4
 #define ech 0
-SRF05 USS(trg, ech);
+uint16_t duration_us;
+float distance;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(trg, OUTPUT);
   while(!Serial){;}
-  delay(1500);
-  Serial.println(USS.getSpeedOfSound());
-  Serial.println(USS.getCorrectionFactor());
-  USS.setModeSingle();
+  delay(3000);
 }
 
 void loop() {
   delay(300);
-  Serial.println(USS.getCentimeter());
+  
 }
